dd059ce72c5fefd61df46d8ffc82c0252b40e7db
pragnesh21 <pragnesh78687@gmail.com>
2023-10-01 12:11:35 +0530

    first commit

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -5,49 +5,122 @@ import "./Contact.css";
 import Rotate from "react-reveal/Rotate";
 import LightSpeed from "react-reveal/LightSpeed";
 import { BsInstagram, BsGithub, BsLinkedin } from "react-icons/bs";
-
 const Contact = () => {
   const [name, setname] = useState("");
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
 
+  //handle submit button
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       if (!name || !email || !msg) {
-        toast.error("Please provide all fields");
+        toast.error("PLease Provide all fields");
+      }
+      const res = await axios.post("/api/v1/portfolio/sendEmail", {
+        name,
+        email,
+        msg,
+      });
+      //validation success
+      if (res.data.success) {
+        toast.success(res.data.message);
+        setname("");
+        setEmail("");
+        setMsg("");
       } else {
-        const response = await axios.post("/api/v1/portfolio/sendWhatsAppMessage", {
-          name,
-          email,
-          msg,
-        });
-
-        if (response.data.success) {
-          toast.success("Message sent to WhatsApp!");
-          // Clear form fields after successful submission
-          setname("");
-          setEmail("");
-          setMsg("");
-        } else {
-          toast.error("Failed to send message to WhatsApp");
-        }
+        toast.error(res.data.message);
       }
     } catch (error) {
-      console.error("Error sending WhatsApp message:", error);
+      console.log(error);
     }
   };
 
   return (
     <>
-      {/* Existing code */}
-      <div className="contact" id="contact">
-        {/* ... (existing code) */}
+      <div className=" contact" id="contact">
+        <div className="card card0 border-0">
+          <div className="row">
+            <div className="col-md-6 col-lg-6 col-xl-6 col-sm-12">
+              <div className="card1">
+                <div className="row border-line">
+                  <LightSpeed>
+                    <img
+                      src="https://img.freepik.com/free-vector/flat-design-illustration-customer-support_23-2148887720.jpg?size=626&ext=jpg&ga=GA1.1.6232676.1690028639&semt=ais"
+                      alt="ocontact"
+                      className="image"
+                    />
+                  </LightSpeed>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6">
+              <Rotate>
+                <div className="card2 d-flex card border-0 px-4 py-5">
+                  <div className="row">
+                    <div className="row">
+                    <h6>
+          Contact With
+          <a href="https://www.linkedin.com/in/pragnesh-makwana-6812651b4/" target="_blank" rel="noopener noreferrer">
+            <BsLinkedin color="blue" size={30} className="ms-2" />
+          </a>
+          &nbsp;&nbsp;
+          <a href="https://github.com/pragnesh21" target="_blank" rel="noopener noreferrer">
+            <BsGithub color="black" size={30} className="ms-2" />
+          </a>
+          &nbsp;&nbsp;
+          <a href="https://www.instagram.com/21_2_pragnesh/" target="_blank" rel="noopener noreferrer">
+            <BsInstagram color="purple" size={30} className="ms-2" />
+          </a>
 
-        <div className="row px-3">
-          <button className="button" onClick={handleSubmit}>
-            SEND MESSAGE
-          </button>
+        </h6>
+                    </div>
+
+                    <div className="row px-3 mb-4">
+                      <div className="line" />
+                      <small className="or text-center">OR</small>
+                      <div className="line" />
+                    </div>
+                    <div className="row px-3">
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter your Name"
+                        className="mb-3"
+                        value={name}
+                        onChange={(e) => setname(e.target.value)}
+                      />
+                    </div>
+                    <div className="row px-3">
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="Enter Your Email Address"
+                        className="mb-3"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <div className="row px-3">
+                      <textarea
+                        type="text"
+                        name="msg"
+                        placeholder="Write your message"
+                        className="mb-3"
+                        value={msg}
+                        onChange={(e) => setMsg(e.target.value)}
+                      />
+                    </div>
+                    <div className="row px-3">
+                      <button className="button" onClick={handleSubmit}>
+                        SEND MESSAGE
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </Rotate>
+            </div>
+          </div>
         </div>
       </div>
     </>
